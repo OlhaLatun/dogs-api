95b4149d5859540aebb8f127e4a598bf92805f1f
Olha Latun <60576067+OlhaLatun@users.noreply.github.com>
2021-05-10 00:51:49 +0300

    Update app.js

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import logo from './img/logo.png'
-import Container from '../Container'
+import Container from '../container'
 import './App.scss'
 
 class HomePageNavigation extends Component {
